6141e32f411a7ace64cc5b5caa8dafe365eba7b9
Lucas Li <sli.lucas233@gmail.com>
2020-06-16 23:19:14 +1200

    commit

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
 var twit = require('twit');
 var config = require('./config.js');
 
-module.exports = {
-    consumer_key: 'NeeOALnuiA9fz1dpmAbm4KQP1',
-    consumer_secret: 'UZjSKYBVulwYCHwwxSHuEO4v7QKyfVkD2lByD2ZYGgIsv0QCZx',
-    access_token: '1272744517272129540-ZWYCKKVXPoeuhcm0Z9l8xNmCqR3iTu',
-    access_token_secret: 'leBy4K5Md7poDLRorvKq4p6ijZ88liWDiC2IOZcarvX8y'
-}   
+module.exports()
